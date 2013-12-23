bb4da77eed85b1d4c63bfcc801f99f08ab60ee8f
aswebdev <andrew.schadendorff@gmail.com>
2013-12-24 09:27:18 +1100

    sections update

--- includes/files/classes.php
+++ includes/files/classes.php
@@ -1,21 +1,21 @@
 <?php
 
-// Create a new instance of a banner
-class Banner {
+// Create a new instance of a slideshow
+class Slideshow {
 	
-	private $images = array(); // the banner images array
-	private $width; // the width of the banner
-	private $height; // the height of the banner
+	private $images = array(); // the slideshow images array
+	private $width; // the width of the slideshow
+	private $height; // the height of the slideshow
 	
 	function __construct($width,$height) {
 		if((empty($width)) || (empty($height))) {
-			throw new Exception("Banner Dimensions need to be set. Width = $width, Height = $height");
+			throw new Exception("Slideshow Dimensions need to be set. Width = $width, Height = $height");
 		}
 		$this->width = $width;
 		$this->height = $height;
 	}
 	
-	// Adds an image file to the banner
+	// Adds an image file to the slideshow
 	function addImage($arr) {
 		// Check the image exists on the server
 		$file = $arr['file'];
@@ -23,15 +23,15 @@ class Banner {
     		if( $fileAtt ==! false ) {
 				// Has a valid image, get the dimensions
 				list($imgWidth,$imgHeight) = $fileAtt;
-				if($imgWidth != $this->$width) { // Width of the image does not match the width of the banner
+				if($imgWidth != $this->$width) { // Width of the image does not match the width of the slideshow
 					echo "File ($fileAtt) width (".$imgWidth.") does not match the width of the banner (".$this->width.").";
 					return false;					
 				}
-				if($imgHeight != $this->$height) { // Width of the image does not match the width of the banner
+				if($imgHeight != $this->$height) { // Width of the image does not match the width of the slideshow
 					echo "File ($fileAtt) height (".$imgheight.") does not match the height of the banner (".$this->height.").";
 					return false;					
 				}
-				// Has validated image with banner. Add it to the Class
+				// Has validated image with slideshow. Add it to the Class
 				$this->images[] = $file;
 			} else {
 				echo "File ($fileAtt) does not exist on the server.";
@@ -40,12 +40,19 @@ class Banner {
 		}
 	}
 	
-	// Outputs the banner on the page
-	function outputBanner() {
+	// Outputs the slideshow on the page
+	function outputSlideshow() {
 		if(is_array($this->images)) {
+			echo "<div class=\"slider\">\n";
 			foreach($this->images as $im) {
-				echo "<img src=\"".$im."\" border=\"0\" width=\"".$this->width."\" height=\"".$this->height."\" />\n";
+				echo "<img src=\"".$im."\" border=\"0\" />\n";
 			}
+			echo "</div>\n";
+			echo "<script>\n";
+			echo "$(window).load(function() {\n";
+    		echo "$('.slider').nivoSlider();\n";
+			echo "});\n";
+			echo "</script>\n";
 		} else {
 			echo "No Files associated with the banner";
 			return false;
